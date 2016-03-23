cee37ca5e7ffa904a68c5b8b9efdb037883a7dd6
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2016-03-23 08:38:29 +0000

    panel test

---
+++ www/js/chitfund-model.js
@@ -0,0 +1,14 @@
+$.ajaxSetup({ cache: false });
+
+var CFModel = window.CFModel || {};
+CFModel.data = CFModel.data || {};
+
+CFModel.initialize = function(){
+    $(function() {
+		init();
+	});
+
+	function init(){
+		
+	}
+}
---
+++ www/js/chitfund-tracker.js
@@ -0,0 +1,15 @@
+$.ajaxSetup({ cache: false });
+
+var CFTracker = window.CFTracker || {};
+CFTracker.dashboard = CFTracker.dashboard || {};
+CFTracker.data = CFTracker.data || {};
+
+CFTracker.dashboard.initialize = function(){
+    $(function() {
+		init();
+	});
+
+	function init(){
+		
+	}
+}
