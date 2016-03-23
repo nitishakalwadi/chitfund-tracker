a6d1d7e3ca036518685d0db9ff76123a321582ca
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2016-03-23 10:27:33 +0000

    testing sqlite

--- www/js/chitfund-tracker.js
+++ www/js/chitfund-tracker.js
@@ -17,13 +17,16 @@ CFTracker.initialize = function(){
 	    CFTracker.db = openDatabase("myApp", 1.0, "App database",200000);
 	    var db = CFT.db;
 	    
+	    //create chit_master table if not exists
 	    db.transaction(function(transaction){
             transaction.executeSql("SELECT table_name FROM sqlite_master WHERE type='table' AND name='chit_master';",
                 [],
                 function(transaction,results){
                     for (var i = 0; i <= results.rows.length; i++) {
-                    var table_name = results.rows.item(i).table_name;
-                    alert(table_name);
+                        var table_name = results.rows.item(i).table_name;
+                        if(!table_name){
+                            transaction.executeSql("CREATE TABLE chit_master (id INTEGER PRIMARY KEY, amt INTEGER, month INTEGER, note VARCHAR);");
+                        }
                     }
                 },
                 function(e){
