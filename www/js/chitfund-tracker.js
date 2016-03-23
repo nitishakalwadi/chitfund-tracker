8accc94eede03a890be5b0ff888bcd4438eff40e
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2016-03-23 12:52:26 +0000

    testing sqlite, minor bug

--- www/js/chitfund-tracker.js
+++ www/js/chitfund-tracker.js
@@ -17,34 +17,17 @@ CFTracker.initialize = function(){
 	    CFTracker.db = openDatabase("myApp", 1.0, "App database",200000);
 	    var db = CFTracker.db;
 	    
-	    //create chit_master table if not exists
+	    //create chit_master and chit_transaction table if not exists
 	    db.transaction(function(transaction){
-	        transaction.executeSql("DROP TABLE IF EXISTS chit_master;");
-	        transaction.executeSql("DROP TABLE IF EXISTS chit_transaction;");
+	        transaction.executeSql("DROP TABLE chit_master");
+	        transaction.executeSql("DROP TABLE chit_transaction");
 	        transaction.executeSql("CREATE TABLE IF NOT EXISTS chit_master (id INTEGER PRIMARY KEY, name VARCHAR, monthly_premium INTEGER, months INTEGER, commission INTEGER);");
 	        transaction.executeSql("CREATE TABLE IF NOT EXISTS chit_transaction (id INTEGER PRIMARY KEY, chit_id INTEGER, bid_amount INTEGER);");
-	        // transaction.executeSql("CREATE TABLE chit_master (id INTEGER PRIMARY KEY, name VARCHAR, monthly_premium INTEGER, months INTEGER, commission INTEGER);");
-            // transaction.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='chit_master';",
-            //     [],
-            //     function(transaction,results){
-            //         for (var i = 0; i <= results.rows.length; i++) {
-            //             var table_name = results.rows.item(i).name;
-            //             alert(table_name);
-            //             if(!table_name){
-            //                 transaction.executeSql("CREATE TABLE chit_master (id INTEGER PRIMARY KEY, name VARCHAR, monthly_premium INTEGER, months INTEGER, commission INTEGER);");
-            //             }
-            //         }
-            //     },
-            //     function(err){
-            //         alert(err);
-            //     }
-            // );
-            //transaction.executeSql("CREATE TABLE chit_transaction (id INTEGER PRIMARY KEY, chit_id INTEGER, bid_amount INTEGER);");
-            //transaction.executeSql("INSERT INTO chit_master (amt,months,note values (?,?,?);",[123,456,"test"]);
-            transaction.executeSql("INSERT INTO chit_master (name,monthly_premium,months,commission) values ('test',111,222,10);");
-            transaction.executeSql("INSERT INTO chit_master (name,monthly_premium,months,commission) values ('test',222,222,10);");
-            
-        });
+	    });
+	    
+	    db.transaction(function(transaction){
+	        transaction.executeSql("INSERT INTO chit_master (name,monthly_premium,months,commission) values (?,?,?,?);",["name",1000,1,10]);
+	    });
         
         db.transaction(function(transaction){alert(123);
                 transaction.executeSql("SELECT * from chit_master",
