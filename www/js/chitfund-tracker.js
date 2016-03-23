70665d7ee7073fdc9db14c0a7589858aadfa700c
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2016-03-23 11:22:32 +0000

    testing sqlite, minor bug

--- www/js/chitfund-tracker.js
+++ www/js/chitfund-tracker.js
@@ -19,27 +19,38 @@ CFTracker.initialize = function(){
 	    
 	    //create chit_master table if not exists
 	    db.transaction(function(transaction){
-            transaction.executeSql("CREATE TABLE chit_master (id INTEGER PRIMARY KEY, amt INTEGER, months INTEGER, note VARCHAR);");
+            transaction.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='chit_master';",
+                [],
+                function(transaction,results){
+                    for (var i = 0; i <= results.rows.length; i++) {
+                        var table_name = results.rows.item(i).name;
+                        alert(table_name);
+                    }
+                },
+                function(err){
+                    
+                }
+            );
             //transaction.executeSql("CREATE TABLE chit_transaction (id INTEGER PRIMARY KEY, chit_id INTEGER, bid_amount INTEGER);");
             //transaction.executeSql("INSERT INTO chit_master (amt,months,note values (?,?,?);",[123,456,"test"]);
-            transaction.executeSql("INSERT INTO chit_master (amt,months,note) values (111,222,'test');");
+            //transaction.executeSql("INSERT INTO chit_master (amt,months,note) values (111,222,'test');");
             
         });
         
-        db.transaction(function(transaction){alert(123);
-                transaction.executeSql("SELECT * from chit_master",
-                    [],
-                    successCallback,
-                    function(err){
-                        alert("some error "+err.message);
-                    })
-            });
+        // db.transaction(function(transaction){alert(123);
+        //         transaction.executeSql("SELECT * from chit_master",
+        //             [],
+        //             successCallback,
+        //             function(err){
+        //                 alert("some error "+err.message);
+        //             })
+        //     });
             
-            function successCallback(transaction,results){
-                        for (var i = 0; i <= results.rows.length; i++) {
-                            var amount = results.rows.item(i).amt;
-                            alert(amount);
-                        }
-                    }
+        //     function successCallback(transaction,results){
+        //                 for (var i = 0; i <= results.rows.length; i++) {
+        //                     var amount = results.rows.item(i).amt;
+        //                     alert(amount);
+        //                 }
+        //             }
 	}
 }
