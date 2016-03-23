00090df3d0a5dffc05cdc63c8acb264010ebcfcf
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2016-03-23 10:07:40 +0000

    transition test

--- www/js/chitfund-tracker.js
+++ www/js/chitfund-tracker.js
@@ -1,21 +1,15 @@
 $.ajaxSetup({ cache: false });
 
 var CFTracker = window.CFTracker || {};
-CFTracker.dashboard = CFTracker.dashboard || {};
+
 CFTracker.data = CFTracker.data || {};
 
-CFTracker.dashboard.initialize = function(){
+CFTracker.initialize = function(){
     $(function() {
 		init();
 	});
 
 	function init(){
-		initBindings();
-	}
-	
-	function initBindings(){
-	    $('.ui-btn').on('tap', function(){
-	        alert(123);
-	    });
+		
 	}
 }
