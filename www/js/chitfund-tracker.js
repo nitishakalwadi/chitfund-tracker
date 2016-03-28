0f646ac05c16e465be192fc0d376c9f24e45cac7
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2016-03-28 10:49:04 +0000

    sqlite test

--- www/js/chitfund-tracker.js
+++ www/js/chitfund-tracker.js
@@ -62,7 +62,7 @@ CFTracker.dashboard.initialize = function(){
         	var markup = "";
         	// markup += "<ul id='dashboardListView' data-role='listview'>";
         	for (var i = 0; i <= results.rows.length; i++) {
-        		var name = results.rows.item(i).name;alert(name);
+        		var name = results.rows.item(i).name;alert(name);typeof(name);
         		// var monthly_premium = results.rows.item(i).monthly_premium;alert(monthly_premium);
         		// var months = results.rows.item(i).months;alert(months);
         		// var commission = results.rows.item(i).commission;alert(commission);
@@ -71,9 +71,8 @@ CFTracker.dashboard.initialize = function(){
         		markup += "<a href='#'>"+name+"</a>";
         		markup += "</li>";
         	}
-        	markup += "</ul>";
         	// markup = "hello man";
-        	// alert(markup);
+        	 alert(markup);
         	$("#dashboardListView").html(markup);
         	$("#dashboardListView").listview( "refresh" );
         }
