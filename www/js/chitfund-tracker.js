16ebb9379f5832b85c79bac42241b5252c487121
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2016-03-28 13:04:09 +0000

    sqlite test

--- www/js/chitfund-tracker.js
+++ www/js/chitfund-tracker.js
@@ -19,8 +19,8 @@ CFTracker.dashboard.initialize = function(){
 	    
 	    //create chit_master and chit_transaction table if not exists
 	    db.transaction(function(transaction){
-	        transaction.executeSql("DROP TABLE IF EXISTS chit_master");
-	        transaction.executeSql("DROP TABLE IF EXISTS chit_transaction");
+	        //transaction.executeSql("DROP TABLE IF EXISTS chit_master");
+	        //transaction.executeSql("DROP TABLE IF EXISTS chit_transaction");
 	        transaction.executeSql("CREATE TABLE IF NOT EXISTS chit_master (id INTEGER PRIMARY KEY, name VARCHAR, monthly_premium INTEGER, months INTEGER, commission INTEGER);");
 	        transaction.executeSql("CREATE TABLE IF NOT EXISTS chit_transaction (id INTEGER PRIMARY KEY, chit_id INTEGER, bid_amount INTEGER);");
 	    });
@@ -113,15 +113,15 @@ CFTracker.addChit.initialize = function(){
 	}
 	
 	function initBtns(){
-		$("#save").on("tap", function(){
+		$("#save").on("tap", function(){alert(123);
 			var chitname = $("#chitname").val();
 			var monthlyPremium = $("#monthlyPremium").val();
 			var months = $("#months").val();
 			var commission = $("#commission").val();
 			var insertArr = new Array(chitname, monthlyPremium, months, commission);
 			
-			CFTracker.db = openDatabase("myApp", 1.0, "App database",200000);
-			var db = CFTracker.db;
+			//CFTracker.db = openDatabase("myApp", 1.0, "App database",200000);
+			var db = openDatabase("myApp", 1.0, "App database",200000);
 			db.transaction(function(transaction){
 	        	transaction.executeSql("INSERT INTO chit_master (name,monthly_premium,months,commission) values (?,?,?,?);", insertArr);
 	    	});
