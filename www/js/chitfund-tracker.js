1c2716bfe8c50c890600d7d5360f26c54a9d8e0a
Nitish Akalwadi <nitish_akalwadi@yahoo.co.in>
2016-03-29 11:50:18 +0000

    sqlite test

--- www/js/chitfund-tracker.js
+++ www/js/chitfund-tracker.js
@@ -19,8 +19,8 @@ CFTracker.dashboard.initialize = function(){
 	    
 	    //create chit_master and chit_transaction table if not exists
 	    db.transaction(function(transaction){
-	        transaction.executeSql("DROP TABLE IF EXISTS chit_master");
-	        transaction.executeSql("DROP TABLE IF EXISTS chit_transaction");
+	        //transaction.executeSql("DROP TABLE IF EXISTS chit_master");
+	        //transaction.executeSql("DROP TABLE IF EXISTS chit_transaction");
 	        transaction.executeSql("CREATE TABLE IF NOT EXISTS chit_master (id INTEGER PRIMARY KEY, chitname VARCHAR, monthly_premium INTEGER, months INTEGER, commission INTEGER);");
 	        transaction.executeSql("CREATE TABLE IF NOT EXISTS chit_transaction (id INTEGER PRIMARY KEY, chit_id INTEGER, bid_amount INTEGER);");
 	    });
